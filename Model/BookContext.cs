17cdb7f2025a43c217189f8eb81f372ca2a76ac5
Сергей <muromtsev1996@gmail.com>
2022-10-04 22:10:54 +0300

    Добавил начальные данные(без картинки, картинку можно добавить из приложения)

--- Model/BookContext.cs
+++ Model/BookContext.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using Microsoft.EntityFrameworkCore;
 
 namespace TestBook.Model;
@@ -13,12 +14,23 @@ public class BookContext : DbContext
     {
         Database.EnsureCreated();
     }
+
     public BookContext(DbContextOptions<BookContext> options) : base(options)
     {
         Database.EnsureCreated();
     }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlServer($"Server=DESKTOP-HCJS4U4; Database={path}\\BookDb;Trusted_Connection=True;"); 
+        => options.UseSqlServer($"Server=DESKTOP-HCJS4U4; Database={path}\\BookDb;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>().HasData(
+            new Book[] 
+            {
+                new Book { Id=1, Name="Руслан и людмила", Date="1813", ISBN="166-000-22", Description="jdjJJj", Authors = new List<Author>(){ new Author(){ Id=1,Name = "Пушкин"}}},
+                new Book { Id=2, Name="Война и мир", Date="1813", ISBN="166-000-22", Description="jdjAAAj", Authors = new List<Author>(){ new Author(){ Id=1,Name = "Пушкин" }}},
+                new Book { Id=3, Name="Пиковая дама", Date="1813", ISBN="166-000-22", Description="jdjAAj", Authors = new List<Author>(){ new Author(){ Id=1,Name = "Пушкин" }}}
+            });
+    }
 }
-    
